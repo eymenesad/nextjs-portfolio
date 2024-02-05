39c3e544de6bb58b41ec90d78da7888e15112ee8
eymenesad <eymenesad123@gmail.com>
2024-02-05 14:41:37 +0300

    h

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -15,8 +15,6 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
         >
-
-
           <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
             <span className="text-white bg-clip-text bg-gradient-to-r ">
               Hello, I&apos;m{" "}
@@ -65,7 +63,4 @@ const HeroSection = () => {
     </section>
   );
 };
-
 export default HeroSection;
-
-
